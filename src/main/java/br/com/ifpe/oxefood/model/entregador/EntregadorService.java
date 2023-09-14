9627573a4b000091934dbd184e37c0e740bce1a8
aluno <aluno@ifpe>
2023-09-14 11:37:34 -0300

    Aula dia 14/09 - Implementação da Alteração dos Dados

--- src/main/java/br/com/ifpe/oxefood/model/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/model/entregador/EntregadorService.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class EntregadorService {
     
@@ -30,6 +31,31 @@ public class EntregadorService {
     public Entregador findById(Long id) {
         return repository.findById(id).get();
     }
+
+    @Transactional
+    public void update(Long id, Entregador entregadorAlterado) {
+ 
+       Entregador entregador = repository.findById(id).get();
+       entregador.setNome(entregadorAlterado.getNome());
+       entregador.setCpf(entregadorAlterado.getCpf());
+       entregador.setRg(entregadorAlterado.getRg());
+       entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+       entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+       entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+       entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+       entregador.setValorFrete(entregadorAlterado.getValorFrete());
+       entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+       entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
+       entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+       entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+       entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+       entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
+       entregador.setEnderecoComplemento(entregadorAlterado.getEnderecoComplemento());
+       entregador.setAtivo(entregadorAlterado.getAtivo());
+         
+       entregador.setVersao(entregador.getVersao() + 1);
+       repository.save(entregador);
+   }
 
 }
 
